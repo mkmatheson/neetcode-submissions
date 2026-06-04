9458749d6d470a922ab8095278deb788af6e1549
mkmatheson <48038833+mkmatheson@users.noreply.github.com>
2026-06-04 08:03:17 -0600

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.ts
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let encodedString = '';
+        for(const str of strs){
+            encodedString += `${str.length}#${str}`;
+        }
+        return encodedString
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const strings = [];
+        let stringLength = 0;
+        let currString = '';
+        let i = 0;
+        while(i < str.length){
+            let stringifiedLength = '';
+            while(str[i] !== '#'){
+                stringifiedLength += str[i]
+                i++;
+            }
+            i++;
+            stringLength = parseInt(stringifiedLength);
+            while(stringLength > 0)
+            {
+                currString += str[i];
+                stringLength--;
+                i++;
+            }
+
+            strings.push(currString);
+            currString = '';
+            stringLength = 0;
+        }
+        return strings;
+    }
+}
